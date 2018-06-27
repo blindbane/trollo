73152b19a1cb780643b357d8226ecca8169d9c1b
Aidan Bane <aidanfbane@gmail.com>
2018-06-26 21:10:17 -0700

    (feat) init connection to mongoDB and troll model

---
+++ server/db/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Troll = require('./models/Troll')
+mongoose.connect(process.env.MONGO_URI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to db')
+});
+
+var trollBill = new Troll({ body: 'What a troll' });
+
+trollBill.save((err, troll) => {
+  if (err) return console.error(err);
+  return;
+});
+
+Troll.find((err, trollBill) => {
+  if (err) return console.error(err);
+  console.log(trollBill);
+})
+
+Troll.find({ body: /^What/ }, (err, troll) => {
+  if (err) return console.error(err);
+  console.log("found a troll  ", troll);
+});
+
--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./db');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/trolls', (req, res) =>
   res.send({
     trolls: [
       {
         id: 1,
-        description: 'the best troll'
+        description: 'the best troll',
       },
       {
         id: 2,
-        description: 'the second best troll'
-      }
-    ]
-  })
-)
+        description: 'the second best troll',
+      },
+    ],
+  }),
+);
 
-const PORT = process.env.PORT || 1337
-app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`))
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`));
