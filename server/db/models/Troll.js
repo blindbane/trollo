a95b882223649c84c242e1502bec7fbd373104c4
Aidan Bane <aidanfbane@gmail.com>
2018-06-26 21:10:17 -0700

    (feat) init connection to mongoDB and troll model

---
+++ server/db/models/Troll.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const trollSchema = mongoose.Schema({
+  body: {
+    type: String,
+    require: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Troll = mongoose.model('Troll', trollSchema);
+
+module.exports = Troll;
