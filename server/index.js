ccbda026529e6899e4db4a4ccac74b65a5425cb9
Aidan Bane <aidanfbane@gmail.com>
2018-06-26 20:34:20 -0700

    (feat) Add dotenv setup and example

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/api/trolls', (req, res) =>
   res.send({
     trolls: [
       {
         id: 1,
-        description: 'the best troll',
+        description: 'the best troll'
       },
       {
         id: 2,
-        description: 'the second best troll',
-      },
-    ],
-  }),
-);
+        description: 'the second best troll'
+      }
+    ]
+  })
+)
 
-const PORT = process.env.PORT || 1337;
-app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 1337
+app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`))
