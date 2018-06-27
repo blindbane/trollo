0bc07cf313e0bb31f8a2955ccdb5d712bf54c554
aidanbane <aidanfbane@gmail.com>
2018-06-26 18:34:38 -0700

    (feat) Added Proxy and fake API route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/trolls', (req, res) =>
+  res.send({
+    trolls: [
+      {
+        id: 1,
+        description: 'the best troll',
+      },
+      {
+        id: 2,
+        description: 'the second best troll',
+      },
+    ],
+  }),
+);
 
 const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`));
