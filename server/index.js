bcbba76c53393f6dbbdecd5ec2570cdcbe383af7
aidanbane <aidanfbane@gmail.com>
2018-06-26 17:57:58 -0700

    (feat) Hello,world express

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => console.log(`Server is now listening on http://localhost:${PORT}`))
