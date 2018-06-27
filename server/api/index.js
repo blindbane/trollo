09520f70f62b6165f6e8315caa687b270a34e83e
Aidan Bane <aidanfbane@gmail.com>
2018-06-26 22:41:03 -0700

    (feat) Add POST route for new Troll

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const trolls = require('./trolls')
+const trolls = require('./trolls');
 
-router.use('/trolls', trolls)
+router.use('/trolls', trolls);
 
 module.exports = router;
--- server/api/trolls.js
+++ server/api/trolls.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const trollCtrl = require('../controllers/trollCtrl');
 
-router.get('/', trollCtrl.getAllTrolls);
+router.get('/', trollCtrl.getAllTrolls).post('/', trollCtrl.addNewTroll);
 
 module.exports = router;
