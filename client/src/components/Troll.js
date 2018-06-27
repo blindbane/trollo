eb24a54bbf55d20526fcd72c442481f817c36552
Aidan Bane <aidanfbane@gmail.com>
2018-06-27 00:30:44 -0700

    (feat) Updated labels for troll ui

--- client/src/components/Troll.js
+++ client/src/components/Troll.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Troll = ({ body, title }) => {
   return (
-    <div>
-      <div>{title}</div>
-      <div>{body}</div>
+    <div style={{ border: '1px solid black' }}>
+      <div>Title: {title}</div>
+      <div>The troll: {body}</div>
     </div>
   )
 };
