e03c31aa209d890f1157fc2ab5384b61e1a5c853
Aidan Bane <aidanfbane@gmail.com>
2018-06-27 00:29:13 -0700

    (feat) Added title to controller

--- client/src/components/Troll.js
+++ client/src/components/Troll.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Troll = ({ body }) => {
-  return <div>{body}</div>;
+const Troll = ({ body, title }) => {
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{body}</div>
+    </div>
+  )
 };
 
 export default Troll;
