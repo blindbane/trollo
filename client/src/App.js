175baf264e813d554afe79109a5190e8d5fa83e7
aidanbane <aidanfbane@gmail.com>
2018-06-26 18:47:49 -0700

    (feat) Init render of API data on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+import TrollList from './TrollList';
+
 class App extends Component {
+  state = {
+    trolls: [],
+  };
+  componentDidMount() {
+    axios
+      .get('/api/trolls')
+      .then(res => res.data)
+      .then(({ trolls }) => {
+        this.setState({
+          trolls,
+        });
+      });
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +27,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.trolls ? <TrollList trolls={this.state.trolls} /> : <h1>FETCHING DATA</h1>}
         </p>
       </div>
     );
